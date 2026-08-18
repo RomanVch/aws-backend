f68ba26ec8e593819da0b6b4a17c8aed98e5a1d0
RomanVch <romavyacheslavovich@gmail.com>
2026-08-18 19:32:20 +0400

    hw-8

--- src/readiness.service.ts
+++ src/readiness.service.ts
@@ -9,7 +9,7 @@ export class ReadinessService implements OnApplicationBootstrap {
     // имитация долгой инициализации (прогрев кэша, подключение к БД и т.п.)
     setTimeout(() => {
       this.ready = true;
-    }, 15000); // через 15 секунд станет ready
+    }, 15000); // через 15 секунд станет read
   }
 
   isReady(): boolean {
